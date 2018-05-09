882d113c46b60b7b2391a1ae556fba4f1bcf8959
Ben Thompson <t.ben.thompson@gmail.com>
2018-05-09 17:51:42 -0400

    tris vs rects

---
+++ tests/rects.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tris import gaussian, gauss_params, sm, nu, plot_tris
+from tris import main as tri_main
+from tectosaur.mesh.mesh_gen import make_rect, rect_points
+
+import tde.fullspace
+
+n = 50
+corners = [[-1, 0, 1], [-1, 0, -1], [1, 0, -1], [1, 0, 1]]
+
+def dofs_to_tris(field, factor = 3):
+    n_rects = field.shape[0]
+    out = np.tile(field.reshape((n_rects, -1))[:,np.newaxis,:], (1,factor,1))
+    out_shape = list(field.shape)
+    out_shape[0] *= factor
+    return out.reshape(out_shape)
+
+def build_weird_tri_mesh(nx, ny, corners):
+    true_nx = 2 * nx - 1
+    x = np.linspace(0, 1, 2 * nx - 1)
+    y = np.linspace(0, 1, ny)
+    pts = rect_points(corners, x, y)
+
+    def v_idx(i, j):
+        return j * true_nx + i * 2
+
+    def midpt_idx(i, j):
+        return j * true_nx + i * 2 + 1
+
+    tris = []
+    rects = []
+    for i in range(nx - 1):
+        for j in range(ny - 1):
+            top_left = v_idx(i, j)
+            top_right = v_idx(i + 1, j)
+            bottom_left = v_idx(i, j + 1)
+            bottom_right = v_idx(i + 1, j + 1)
+            top_middle = midpt_idx(i, j)
+            tris.append([top_left, bottom_left, top_middle])
+            tris.append([top_middle, bottom_right, top_right])
+            tris.append([bottom_left, bottom_right, top_middle])
+            rects.append([top_left, bottom_left, bottom_right, top_right])
+    tris = np.array(tris, dtype = np.int)
+    rects = np.array(rects, dtype = np.int)
+    return pts, tris, rects
+
+def main():
+    pts, tris, rects = build_weird_tri_mesh(n, n, corners)
+
+    n_tris = tris.shape[0]
+    n_rects = rects.shape[0]
+    assert(3 * n_rects == n_tris)
+    tri_pts = pts[tris]
+    rect_pts = pts[rects]
+    rect_centers = np.mean(rect_pts, axis = 1)
+    dist = np.linalg.norm(rect_centers, axis = 1)
+
+    plt.figure(figsize = (15,15))
+    plt.triplot(pts[:,0], pts[:,2], tris)
+    plt.plot(rect_centers[:,0], rect_centers[:,2], '*')
+    plt.savefig('rect_setup.pdf')
+    # plt.show()
+
+
+    strike_slip = gaussian(*gauss_params, dist)
+    slip = np.zeros((strike_slip.shape[0], 3))
+    slip[:,0] = strike_slip
+    slip_tris = dofs_to_tris(slip)
+
+    plot_tris((pts, tris), slip_tris[:,0], 'inputslip')
+
+    strain = np.empty((n_rects, 6))
+    for i in range(n_rects):
+        obs_pts = np.tile(rect_centers[i,np.newaxis,:], (n_tris, 1))
+        all_strains = tde.fullspace.clu_strain(obs_pts, tri_pts, slip_tris, nu)
+        strain[i, :] = np.sum(all_strains, axis = 0)
+    stress = tde.fullspace.strain_to_stress(strain, sm, nu)
+
+    stress_tris = dofs_to_tris(stress)
+    # for d in range(6):
+    #     plot_tris((pts, tris), stress_tris[:,d])
+    plot_tris((pts, tris), stress_tris[:,3], 'rectsxy')
+    plot_tris((pts, tris), stress_tris[:,5], 'rectsyz')
+
+
+
+if __name__ == "__main__":
+    main()
+    tri_main()
+
+# def mesh_topology(nx, ny):
+#     def v_idx(i, j):
+#         return j * nx + i
+#
+#     rects = []
+#     for i in range(nx - 1):
+#         for j in range(ny - 1):
+#             top_left = v_idx(i, j)
+#             top_right = v_idx(i + 1, j)
+#             bottom_left = v_idx(i, j + 1)
+#             bottom_right = v_idx(i + 1, j + 1)
+#             rects.append([top_left, top_right, bottom_right, bottom_left])
+#     return np.array(rects, dtype = np.int)
+    # pts, tris = make_rect(n, n, corners)
+    # rects = mesh_topology(n, n)
---
+++ tests/tris.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tectosaur.mesh.mesh_gen import make_rect
+
+import tde.fullspace
+
+def gaussian(a, b, c, x):
+    # return np.ones_like(x)
+    return a * np.exp(-((x - b) ** 2) / (2 * c ** 2))
+
+n = 50
+gauss_params = (1.0, 0.0, 0.3)
+sm = 1.0
+nu = 0.25
+
+def plot_tris(m, field, name):
+    plt.figure(figsize = (15,15))
+    plt.title(name)
+    plt.tripcolor(m[0][:,0], m[0][:,2], m[1], field)
+    plt.colorbar()
+    plt.xlabel('x')
+    plt.ylabel('z')
+    plt.savefig(name + '.pdf')
+    # plt.show()
+
+def main():
+    m = make_rect(
+        n, n,
+        [[-1, 0, 1], [-1, 0, -1], [1, 0, -1], [1, 0, 1]]
+    )
+
+    n_tris = m[1].shape[0]
+    tri_pts = m[0][m[1]]
+    tri_centers = np.mean(tri_pts, axis = 1)
+
+    plt.figure(figsize = (15,15))
+    plt.triplot(m[0][:,0], m[0][:,2], m[1])
+    plt.plot(tri_centers[:, 0], tri_centers[:, 2], '*')
+    plt.savefig('tri_setup.pdf')
+    # plt.show()
+
+    dist = np.linalg.norm(tri_centers, axis = 1)
+    strike_slip = gaussian(*gauss_params, dist)
+    slip = np.zeros((strike_slip.shape[0], 3))
+    slip[:,0] = strike_slip
+
+
+    # plt.tricontourf(
+    #     m[0][:,0], m[0][:,2], m[1],
+    #     gaussian(*gauss_params, np.linalg.norm(m[0], axis = 1))
+    # )
+    # plt.triplot(m[0][:,0], m[0][:,2], m[1])
+    # plt.colorbar()
+    # plt.show()
+
+    # tiled_tris = np.tile(tri_pts[np.newaxis, :, :, :], (n_tris, 1, 1, 1)).reshape((-1,3,3))
+    # tiled_slip = np.tile(slip[np.newaxis, :, :], (n_tris, 1, 1)).reshape((-1,3))
+    # tiled_centers = np.tile(tri_centers[:, np.newaxis, :], (1, n_tris, 1)).reshape((-1,3))
+    # strain = tde.fullspace.clu_strain(tiled_centers, tiled_tris, tiled_slip, nu)
+    # stress = tde.fullspace.strain_to_stress(strain, sm, nu)
+    # strain = strain.reshape((n_tris, n_tris, 6))
+    # stress = stress.reshape((n_tris, n_tris, 6))
+    # stress_at_centers = np.sum(stress, axis = 1)
+
+    strain = np.empty((n_tris, 6))
+    for i in range(n_tris):
+        obs_pts = np.tile(tri_centers[i,np.newaxis,:], (n_tris, 1))
+        strain[i, :] = np.sum(
+            tde.fullspace.clu_strain(obs_pts, tri_pts, slip, nu),
+            axis = 0
+        )
+    stress = tde.fullspace.strain_to_stress(strain, sm, nu)
+
+    plot_tris(m, stress[:,3], 'trisxy')
+    plot_tris(m, stress[:,5], 'trisyz')
+
+if __name__ == "__main__":
+    main()
